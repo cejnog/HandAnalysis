c614d59bee1ff453b7dad48cf5e34231a922ba87
cejnog <cejnog@ime.usp.br>
2020-12-23 18:53:20 -0300

    Changed init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 git clone https://github.com/xinghaochen/Pose-REN.git
+mv Pose-REN/* .
+mv Pose-REN/src/demo/* src/demo/
+mv Pose-REN/src/libs/ src/
+mv Pose-REN/src/testing/ src/
+mv Pose-REN/src/utils/ src/
+mv Pose-REN/src/config.py.example src/
+mv Pose-REN/src/__init__.py src/
+mv Pose-REN/src/show_result.py src/
+rm -rf Pose-REN/
